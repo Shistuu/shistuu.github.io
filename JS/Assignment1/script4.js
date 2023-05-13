d1b7561ab1aa1ff9c385c964e2ea5fce98c4bc9f
Shistata Subedi <shistatasubedi7@gmail.com>
2023-05-13 23:29:24 +0545

    Completed JS assignment 1 part 4

--- JS/Assignment1/script4.js
+++ JS/Assignment1/script4.js
@@ -1,13 +1,21 @@
-// Write a function that transforms an array of inputs into a new array based on a provided 
+// Write a function that transforms an array of inputs into a new array based on a provided
 //transformation function.
+
+
 //var numbers = [1, 2, 3, 4];
-
 //function transform(collection, tranFunc) { …TODO }
-
 //var output = transform(numbers, function(num) {
-    //return num * 2;});
+//return num * 2;});
 // output should be [2, 4, 6, 8]
 
+var numbers = [1, 2, 3, 4];
 
+function transform(collection, tranFunc) {
+  return collection.map(tranFunc);
+}
 
-  
+var output = transform(numbers, function (num) {
+  return num * 2;
+});
+
+console.log(output); 
