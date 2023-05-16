3d4329557d7251c94b0831f311a1f8bf3003f490
Shistata Subedi <shistatasubedi7@gmail.com>
2023-05-16 22:43:57 +0545

    made some color changes

--- JS/Ass2/script.js
+++ JS/Ass2/script.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() 
+{
     const carousel = document.querySelector(".carousel");
     const prevBtn = document.querySelector(".prev-btn");
     const nextBtn = document.querySelector(".next-btn");
