8fd1b9bdb907922a35f515b48f6342ac4e20c6e9
Shistata Subedi <shistatasubedi7@gmail.com>
2023-05-16 21:16:50 +0545

    Image carousel

---
+++ JS/Ass2/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector(".carousel");
+    const prevBtn = document.querySelector(".prev-btn");
+    const nextBtn = document.querySelector(".next-btn");
+    const circles = Array.from(document.querySelectorAll(".circle"));
+    const images = Array.from(carousel.getElementsByTagName("img"));
+  
+    let currImgIndex = 0;
+  
+    function updateCarousel() {
+      carousel.style.transform = `translateX(-${currImgIndex * 100}%)`;
+  
+      circles.forEach((circle, index) => {
+        circle.classList.toggle("active", index === currImgIndex);
+      });
+    }
+  
+    function showImage(index) {
+      currImgIndex = index;
+      updateCarousel();
+    }
+  
+    prevBtn.addEventListener("click", function() {
+      currImgIndex = (currImgIndex - 1 + images.length) % images.length;
+      updateCarousel();
+    });
+  
+    nextBtn.addEventListener("click", function() {
+      currImgIndex = (currImgIndex + 1) % images.length;
+      updateCarousel();
+    });
+  
+    circles.forEach((circle, index) => {
+      circle.addEventListener("click", function() {
+        showImage(index);
+      });
+    });
+  
+    updateCarousel();
+  });
+  
