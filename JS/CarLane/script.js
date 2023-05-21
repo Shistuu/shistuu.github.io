a7703b43def5dea2e4c8982d47ccc56292f4e534
Shistata Subedi <shistatasubedi7@gmail.com>
2023-05-21 22:17:05 +0545

    added bullet function

--- JS/CarLane/script.js
+++ JS/CarLane/script.js
@@ -19,7 +19,11 @@ window.onload = () => {
   let keys = {
     ArrowLeft: false,
     ArrowRight: false,
+    Space: false,
   };
+
+  let bullet;
+  let bulletInterval;
 
   function isCollide(a, b) {
     Rectangle_l = a.getBoundingClientRect();
@@ -53,22 +57,41 @@ window.onload = () => {
     let enemy = document.querySelectorAll(".enemy");
     enemy.forEach(function (item) {
       if (isCollide(car, item)) {
-        endGame();
-        gameStart.innerHTML =
-          "Oops! Game Over!! Your current score is " +
-          player.score + "." +
-          "<br>" +
-          " Click here to play again! ";
+        item.style.display = "none";
+        clearInterval(bulletInterval);
+        bullet.style.display = "none";
+        bullet = null;
       }
-
+  
       if (item.y >= 700) {
         item.y = -300;
         item.style.left = Math.floor(Math.random() * 250) + "px";
+        item.style.display = "block";
       }
       item.y += player.speed;
       item.style.top = item.y + "px";
+  
+      // Check for collision between bullet and enemy
+      if (bullet && isCollide(bullet, item)) {
+        item.style.display = "none";
+        clearInterval(bulletInterval);
+        bullet.style.display = "none";
+        bullet = null;
+      }
     });
+  
+    // Generate new obstacle cars
+    if (player.start && enemy.length < 3) {
+      let enemyCar = document.createElement("div");
+      enemyCar.setAttribute("class", "enemy");
+      enemyCar.y = -300;
+      enemyCar.style.top = enemyCar.y + "px";
+      enemyCar.style.backgroundImage = "url('obstacle.png')";
+      enemyCar.style.left = Math.floor(Math.random() * 250) + "px";
+      gameArea.appendChild(enemyCar);
+    }
   }
+  
 
   function gamePlay() {
     let car = document.querySelector(".car");
@@ -87,10 +110,32 @@ window.onload = () => {
       car.style.top = player.y + "px";
       car.style.left = player.x + "px";
 
+      if (keys.Space && !bullet) {
+        shoot();
+      }
+
       window.requestAnimationFrame(gamePlay);
       score.innerText = "Your score:   " + player.score;
       player.score++;
     }
+  }
+
+  function shoot() {
+    bullet = document.createElement("div");
+    bullet.classList.add("bullet");
+    bullet.style.left = player.x + 20 + "px";
+    bullet.style.top = player.y - 10 + "px";
+    gameArea.appendChild(bullet);
+
+    bulletInterval = setInterval(() => {
+      if (bullet && bullet.offsetTop > -50) {
+        bullet.style.top = bullet.offsetTop - player.speed * 2 + "px";
+      } else {
+        clearInterval(bulletInterval);
+        bullet.style.display = "none";
+        bullet = null;
+      }
+    }, 20);
   }
 
   function start() {
@@ -133,10 +178,16 @@ window.onload = () => {
     if (e.code == "ArrowLeft") {
       keys.ArrowLeft = true;
     }
+    if (e.code == "Space") {
+      keys.Space = true;
+    }
   });
   window.addEventListener("keyup", (e) => {
     if (e.code == "ArrowLeft") {
       keys.ArrowLeft = false;
+    }
+    if (e.code == "Space") {
+      keys.Space = false;
     }
   });
 
