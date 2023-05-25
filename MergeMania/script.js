21ec885eb783dc9df13a871dd51f58db28ff760d
Shistata Subedi <shistatasubedi7@gmail.com>
2023-05-25 22:32:50 +0545

    css and started js

---
+++ MergeMania/script.js
@@ -0,0 +1,52 @@
+import Grid from "./Cell.js";
+import Tile from "./tiles.js";
+
+const gameBoard = document.getElementById("game-board");
+
+const grid = new Grid(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+setupInput();
+
+function setupInput() {
+  window.addEventListener("keydown", handleInput, { once: true });
+}
+
+async function handleInput(e) {
+  switch (e.key) {
+    case "ArrowUp":
+      if (!canMoveUp()) {
+        setupInput();
+        return;
+      }
+      await moveUp();
+      break;
+    case "ArrowDown":
+      if (!canMoveDown()) {
+        setupInput();
+        return;
+      }
+      await moveDown();
+      break;
+    case "ArrowLeft":
+      if (!canMoveLeft()) {
+        setupInput();
+        return;
+      }
+      await moveLeft();
+      break;
+    case "ArrowRight":
+      if (!canMoveRight()) {
+        setupInput();
+        return;
+      }
+      await moveRight();
+      break;
+    default:
+      setupInput();
+      return;
+  }
+
+  grid.cells.forEach((cell) => cell.mergeTiles());
+
+}
