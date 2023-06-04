00f82dddd68a04fcaf56c4141ca00d6aae1c1292
Shistata Subedi <shistatasubedi7@gmail.com>
2023-06-05 00:47:41 +0545

    added comments on script file

--- MergeMania/script.js
+++ MergeMania/script.js
@@ -3,32 +3,33 @@ import { initializeLevels } from "./index.js";
 import Grid from "./Grid.js";
 initializeLevels();
 
+//object that has property active set to true
 export var game = {
   active: true,
 };
 // Get the high score from local storage
 let highScore = localStorage.getItem("highScore");
 if (highScore !== null) {
-  highScore = parseInt(highScore, 10);
+  highScore = parseInt(highScore, 10); //convert from string to decimal value
   const highScoreElement = document.getElementById("high-score");
   highScoreElement.innerText = highScore.toString();
 }
 
 const GAMEBOARD = document.getElementById("container");
 
-const GRID = new Grid(GAMEBOARD);
-GRID.randomEmptyCell().tile = new Tile(GAMEBOARD);
-setupInput();
+const GRID = new Grid(GAMEBOARD); //grid should be created inside the gameboard container
+GRID.randomEmptyCell().tile = new Tile(GAMEBOARD); //generates a new tile and places in random cell on the grid
+setupInput(); // handles user input in the game
 
 function setupInput() {
-  window.addEventListener("keydown", handleInput, { once: true });
+  window.addEventListener("keydown", handleInput, { once: true }); // Each time setupInput() is called, a new event listener is registered, and they are not automatically removed after the first trigger.
 }
 
-async function handleInput(e) {
+async function handleInput(e) { //called when a key is pressed
   switch (e.key) {
     case "ArrowUp":
       if (!tile_MoveUp()) {
-        setupInput();
+        setupInput(); //the input setup is re-established before further processing.
         return;
       }
       await Up();
@@ -59,7 +60,7 @@ async function handleInput(e) {
       return;
   }
 
-  GRID.cells.forEach((cell) => cell.mergeTiles());
+  GRID.cells.forEach((cell) => cell.mergeTiles()); //invokes merge tile method on each cell of the grid merging adjacent tiles if possible
 
   const newTile = new Tile(GAMEBOARD);
   GRID.randomEmptyCell().tile = newTile;
@@ -82,22 +83,22 @@ function Right() {
   return slideTiles(GRID.Row.map((row) => [...row].reverse()));
 }
 
-function slideTiles(cells) {
-  return Promise.all(
-    cells.flatMap((group) => {
-      const promises = [];
+function slideTiles(cells) { //handles the sliding of tiles in the specified cells array.
+  return Promise.all( 
+    cells.flatMap((group) => { // maps each element of the array to a new array of promises.
+      const promises = []; //tracks the transition of tiles during the sliding process
       for (let i = 1; i < group.length; i++) {
         const cell = group[i];
         if (cell.tile == null) continue;
         let lastValidCell;
-        for (let j = i - 1; j >= 0; j--) {
-          const moveToCell = group[j];
+        for (let j = i - 1; j >= 0; j--) { //checks backward in the group
+          const moveToCell = group[j]; //moveToCell is the preceeding cell
           if (!moveToCell.canAccept(cell.tile)) break;
           lastValidCell = moveToCell;
         }
 
         if (lastValidCell != null) {
-          promises.push(cell.tile.waitForTransition());
+          promises.push(cell.tile.waitForTransition()); //promise is pushed in array
           if (lastValidCell.tile != null) {
             lastValidCell.mergeTile = cell.tile;
           } else {
@@ -128,10 +129,10 @@ export function tile_MoveLeft() {
 
 export function tile_MoveRight() {
   if (!game.active) return false;
-  return tile_Move(GRID.Row.map((row) => [...row].reverse()));
+  return tile_Move(GRID.Row.map((row) => [...row].reverse())); //This maps each row of the grid and reverses the order of cells within each row, effectively simulating the rightward movement.
 }
 
-function tile_Move(cells) {
+function tile_Move(cells) { //checks if there is any group of cells that contains at least one tile that can be moved
   return cells.some((group) => {
     return group.some((cell, index) => {
       if (index === 0) return false;
