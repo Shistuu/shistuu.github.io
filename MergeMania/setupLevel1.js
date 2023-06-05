82903538986898bd2d0c9c3b33f90b00e3c5e681
Shistata Subedi <shistatasubedi7@gmail.com>
2023-06-05 23:53:45 +0545

    comment in level1

--- MergeMania/setupLevel1.js
+++ MergeMania/setupLevel1.js
@@ -1,34 +1,44 @@
-import {tile_MoveUp, tile_MoveDown, tile_MoveLeft, tile_MoveRight,setupInput, sendGrid } from './script.js';
 import Grid from "./Grid.js";
 import Tile from "./tiles.js";
-
-
+import { 
+  tile_MoveUp,
+  tile_MoveDown,
+  tile_MoveLeft,
+  tile_MoveRight,
+  setupInput,
+  sendGrid,
+} from "./script.js";
 
 export function setupLevel1() {
-  const gridSize=4;
-  const level=1;
+  const gridSize = 4;
+  const level = 1;
   const GAMEBOARD = document.getElementById("container");
-  const GRID = new Grid(GAMEBOARD,gridSize,level);
+
+  // Create a new Grid instance
+  const GRID = new Grid(GAMEBOARD, gridSize, level);
+
+  // Set a random empty cell with a new Tile
   GRID.randomEmptyCell().tile = new Tile(GAMEBOARD);
-  sendGrid(GRID,GAMEBOARD);
+  // Send the grid and gameboard to the server
+  sendGrid(GRID, GAMEBOARD);
   setupInput();
-
-
-  if (
-    !tile_MoveUp(GRID) &&
-    !tile_MoveDown(GRID) &&
-    !tile_MoveLeft(GRID) &&
-    !tile_MoveRight(GRID)
+  debugger;
+  // Check if there are no valid moves
+  if ( 
+    (!tile_MoveUp(GRID) &&
+      !tile_MoveDown(GRID) &&
+      !tile_MoveLeft(GRID) &&
+      !tile_MoveRight(GRID))
   ) {
-    newTile.waitForTransition(true).then(() => {
-      const modal = document.getElementById("game-over-modal");
-      modal.style.display = "flex";
+  
+    const modal = document.getElementById("game-over-modal");
+    modal.style.display = "flex";
 
-      const playAgainBtn = document.getElementById("play-again-btn");
-      playAgainBtn.addEventListener("click", () => {
-        location.reload(); // Refresh the page
-      });
+    const playAgainBtn = document.getElementById("play-again-btn");
+    playAgainBtn.addEventListener("click", () => {
+      location.reload(); // Refresh the page
+      
     });
-    return;
   }
+  return;
 }
