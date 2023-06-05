7384767f5a076de7535eb0fdd7d5c9d9403da429
Shistata Subedi <shistatasubedi7@gmail.com>
2023-06-05 21:39:29 +0545

    comments for formatTime

--- MergeMania/formatTime.js
+++ MergeMania/formatTime.js
@@ -1,7 +1,7 @@
 export function formatTime(time) {
   const MINUTES = Math.floor(time / 60)
     .toString()
-    .padStart(2, "0");
+    .padStart(2, "0"); //ensure that the string representation of minutes is always at least two characters long. If the string is less than two characters, it adds leading zeros ("0") to make it exactly two characters
   const SECONDS = (time % 60).toString().padStart(2, "0");
   return `${MINUTES}:${SECONDS}`;
 }
