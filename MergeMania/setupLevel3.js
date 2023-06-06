3dc06ce77c07a18f86b6f4ec5cd35e54c3c6384e
Shistata Subedi <shistatasubedi7@gmail.com>
2023-06-06 17:17:57 +0545

    added user input in level 3 for the size of grid

--- MergeMania/setupLevel3.js
+++ MergeMania/setupLevel3.js
@@ -12,50 +12,80 @@ import Grid from "./Grid.js";
 import Tile from "./tiles.js";
 
 export function setupLevel3() {
-  const gridSize = 5;
-  const level = 3;
+  const gridSizeInput = document.createElement("input");
+  gridSizeInput.type = "number";
+  gridSizeInput.placeholder = "Enter the number of grids you want to play";
 
-  const GAMEBOARD = document.getElementById("container");
-  const GRID = new Grid(GAMEBOARD, gridSize, level);
-  GRID.randomEmptyCell().tile = new Tile(GAMEBOARD);
-  sendGrid(GRID, GAMEBOARD);
-  setupInput();
+  const submitButton = document.createElement("button");
+  submitButton.textContent = "Start Game";
 
-  const timerDuration = 500; // x seconds
-  let remainingTime = timerDuration;
-  const warningMessage = document.getElementById("warning-message");
+  // Create a container div and append the input and submit button to it
+  const gridbox = document.createElement("div");
 
-  const timerElement = document.getElementById("timer");
-  timerElement.textContent = formatTime(remainingTime);
+  gridbox.appendChild(gridSizeInput);
+  gridbox.appendChild(submitButton);
 
-  const timer = setInterval(() => {
-    remainingTime--;
-    timerElement.textContent = formatTime(remainingTime);
+  // Append the gridbox to the body
+  document.body.appendChild(gridbox);
 
-    if (
-      remainingTime <= 0 ||
-      (!tile_MoveUp() &&
-        !tile_MoveDown() &&
-        !tile_MoveLeft() &&
-        !tile_MoveRight())
-    ) {
-      clearInterval(timer);
+  // Event listener for the submit button
+  submitButton.addEventListener("click", () => {
+    // Get the entered grid size from the input
+    const gridSize = parseInt(gridSizeInput.value);
 
-      const modal = document.getElementById("game-over-modal");
-      modal.style.display = "flex";
-
-      const playAgainBtn = document.getElementById("play-again-btn");
-      playAgainBtn.addEventListener("click", () => {
-        location.reload(); // Refresh the page
-      });
-
-      game.active = false; // Disable tile movement
-
-      return;
-    } else if (remainingTime <= 30 && remainingTime >= 27) {
-      warningMessage.innerHTML = remainingTime + " seconds remaining!";
+    // Check if the entered value is valid
+    if (isNaN(gridSize) || gridSize <= 0) {
+      alert("Invalid grid size. Please enter a positive number.");
     } else {
-      warningMessage.innerHTML = "";
+      // Remove the UI elements from the DOM
+      document.body.removeChild(gridbox);
+
+      // Use the entered grid size to set up the game
+      const level = 3;
+
+      const GAMEBOARD = document.getElementById("container");
+      const GRID = new Grid(GAMEBOARD, gridSize, level);
+      GRID.randomEmptyCell().tile = new Tile(GAMEBOARD);
+      sendGrid(GRID, GAMEBOARD);
+      setupInput();
+
+      const timerDuration = 500; // x seconds
+      let remainingTime = timerDuration;
+      const warningMessage = document.getElementById("warning-message");
+
+      const timerElement = document.getElementById("timer");
+      timerElement.textContent = formatTime(remainingTime);
+
+      const timer = setInterval(() => {
+        remainingTime--;
+        timerElement.textContent = formatTime(remainingTime);
+
+        if (
+          remainingTime <= 0 ||
+          (!tile_MoveUp() &&
+            !tile_MoveDown() &&
+            !tile_MoveLeft() &&
+            !tile_MoveRight())
+        ) {
+          clearInterval(timer);
+
+          const modal = document.getElementById("game-over-modal");
+          modal.style.display = "flex";
+
+          const playAgainBtn = document.getElementById("play-again-btn");
+          playAgainBtn.addEventListener("click", () => {
+            location.reload(); // Refresh the page
+          });
+
+          game.active = false; // Disable tile movement
+
+          return;
+        } else if (remainingTime <= 30 && remainingTime >= 27) {
+          warningMessage.innerHTML = remainingTime + " seconds remaining!";
+        } else {
+          warningMessage.innerHTML = "";
+        }
+      }, 1000);
     }
-  }, 1000);
+  });
 }
