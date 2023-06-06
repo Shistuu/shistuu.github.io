b80deb9becb3467c15f240a0195bb123b89e6af7
Shistata Subedi <shistatasubedi7@gmail.com>
2023-06-07 00:01:51 +0545

    made some changes in level 3

--- MergeMania/setupLevel3.js
+++ MergeMania/setupLevel3.js
@@ -1,6 +1,6 @@
 import { formatTime } from "./formatTime.js";
-import { game } from "./script.js";
 import {
+  game,
   tile_MoveUp,
   tile_MoveDown,
   tile_MoveLeft,
@@ -11,17 +11,24 @@ import {
 import Grid from "./Grid.js";
 import Tile from "./tiles.js";
 
+// Function to set up Level 3
 export function setupLevel3() {
+  // Create the necessary elements
   const gridSizeInput = document.createElement("input");
   gridSizeInput.type = "number";
   gridSizeInput.placeholder = "Enter the number of grids you want to play";
+
+  const TIMER = document.getElementById("timer");
+  TIMER.style.display = "none";
+
+  const SCORE_CONTAINER = document.getElementById("score-container");
+  SCORE_CONTAINER.style.display = "none";
 
   const submitButton = document.createElement("button");
   submitButton.textContent = "Start Game";
 
   // Create a container div and append the input and submit button to it
   const gridbox = document.createElement("div");
-
   gridbox.appendChild(gridSizeInput);
   gridbox.appendChild(submitButton);
 
@@ -34,15 +41,16 @@ export function setupLevel3() {
     const gridSize = parseInt(gridSizeInput.value);
 
     // Check if the entered value is valid
-    if (isNaN(gridSize) || gridSize <= 0) {
-      alert("Invalid grid size. Please enter a positive number.");
+    if (isNaN(gridSize) || gridSize <= 4 ||gridSize>=10) {
+      alert("Invalid grid size. Please enter a number between  5 to 10.");
     } else {
       // Remove the UI elements from the DOM
       document.body.removeChild(gridbox);
+      TIMER.style.display = "block";
+      SCORE_CONTAINER.style.display = "flex";
 
       // Use the entered grid size to set up the game
       const level = 3;
-
       const GAMEBOARD = document.getElementById("container");
       const GRID = new Grid(GAMEBOARD, gridSize, level);
       GRID.randomEmptyCell().tile = new Tile(GAMEBOARD);
@@ -78,14 +86,14 @@ export function setupLevel3() {
           });
 
           game.active = false; // Disable tile movement
-
-          return;
         } else if (remainingTime <= 30 && remainingTime >= 27) {
           warningMessage.innerHTML = remainingTime + " seconds remaining!";
         } else {
           warningMessage.innerHTML = "";
         }
       }, 1000);
+
+ 
     }
   });
 }
