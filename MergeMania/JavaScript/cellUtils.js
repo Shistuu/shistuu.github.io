3738c0132708427bce3045d16d1d8adb5855ce46
Shistata Subedi <shistatasubedi7@gmail.com>
2023-06-07 18:40:47 +0545

    code

--- MergeMania/JavaScript/cellUtils.js
+++ MergeMania/JavaScript/cellUtils.js
@@ -3,8 +3,8 @@ export function createCellElements(gridElement,RANDOM_INDEX, girdSize, level) {
   for (let i = 0; i < girdSize * girdSize; i++) { //loop runs for each cell position in the grid
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    for(let j=0; j< RANDOM_INDEX.length;j++){
-      const coordinates = RANDOM_INDEX[j];
+    for(const element of RANDOM_INDEX){
+      const coordinates = element;
       if( coordinates[0] == i % girdSize && coordinates[1] == Math.floor(i / girdSize) && level == 3) {
         cell.classList.add("obstacleBorder");
         break;
